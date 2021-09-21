0137c794d8f07f30d0d2c9989c4c6da92ae6078e
FernandoFigueroa14 <luisfersurfer14@hotmail.com>
2021-09-21 15:04:08 -0500

    config

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -17,17 +17,19 @@ module.exports = {
     "dialect": "mysql"
   },*/
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
+    "username": "admin",
+    "password": 'PsycoInova-21',
+    "database": "coralio",
+    "host": "coralio.ccr63migrrcg.us-east-1.rds.amazonaws.com",
+    "port": "3306",
     "dialect": "mysql"
-  },
+ },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": "admin",
+    "password": 'PsycoInova-21',
+    "database": "coralio",
+    "host": "coralio.ccr63migrrcg.us-east-1.rds.amazonaws.com",
+    "port": "3306",
     "dialect": "mysql"
-  }
+ }
 }
